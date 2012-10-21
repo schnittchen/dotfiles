08000f6473696cf644cfc100bb2df44e55a4cb2a
Thomas Stratmann <thomas.stratmann@9elements.com>
2012-10-21 17:59:19 +0200

    new bash alias attach-to-tmux-session

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -7,3 +7,5 @@ alias unpause_guards="psgrep -n bin/guard | cut -f 2 -d ' ' | xargs kill -USR2"
 
 alias rserver='bundle exec rails server -p $((0x`pwd | md5 | cut -b 1-4` % (65536 - 1024) + 1024))'
 alias amend='git commit --amend -C HEAD'
+
+alias attach-to-tmux-session='tmux new-session -t'
