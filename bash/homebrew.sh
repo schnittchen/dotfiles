7f2d8fefb62650e8507d2a3858c179c065634101
Thomas Stratmann <thomas.stratmann@9elements.com>
2013-01-08 10:19:37 +0100

    /usr/local/bin must be in the PATH for homebrew

--- bash/homebrew.sh
+++ bash/homebrew.sh
@@ -1,4 +1,4 @@
-export PATH=/usr/local/bin:$PATH
+export PATH=/usr/local/bin:/usr/local/sbin:$PATH
 export MANPATH=/usr/local/share/man:$MANPATH
 if [ -f `brew --prefix`/etc/bash_completion ]; then
   . `brew --prefix`/etc/bash_completion
