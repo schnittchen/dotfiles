34126cb5510a391f88947df1a65761f5de1b9af5
Thomas Stratmann <thomas.stratmann@9elements.com>
2013-12-29 01:16:41 +0100

    no homebrew stuff when no brew

--- bash/homebrew.sh
+++ bash/homebrew.sh
@@ -1,3 +1,5 @@
+hash brew 2> /dev/null || return
+
 export PATH=/usr/local/bin:/usr/local/sbin:$PATH
 export MANPATH=/usr/local/share/man:$MANPATH
 if [ -f `brew --prefix`/etc/bash_completion ]; then
