a862feacd686bd1bd376aaad9639e60bb444217c
Thomas Stratmann <thomas.stratmann@9elements.com>
2013-12-29 02:10:46 +0100

    provide which_ruby for chruby

--- bash/chruby.sh
+++ bash/chruby.sh
@@ -1,1 +1,7 @@
-[ -e /usr/local/share/chruby/chruby.sh ] && source /usr/local/share/chruby/chruby.sh
+[ -e /usr/local/share/chruby/chruby.sh ] || return
+
+source /usr/local/share/chruby/chruby.sh
+
+function which_ruby(){
+  chruby | grep \* | cut -b 4-
+}
