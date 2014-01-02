365cd5492e9a815673f94c8058937405d142ed61
Thomas Stratmann <thomas.stratmann@9elements.com>
2014-01-02 03:54:40 +0100

    try chruby auto-switching

--- bash/chruby.sh
+++ bash/chruby.sh
@@ -1,6 +1,7 @@
 [ -e /usr/local/share/chruby/chruby.sh ] || return
 
 source /usr/local/share/chruby/chruby.sh
+source /usr/local/share/chruby/auto.sh
 
 function which_ruby(){
   chruby | grep \* | cut -b 4-
