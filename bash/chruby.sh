157c8a3c5dfd10bc7c4f8b1604010300739a97f2
Thomas Stratmann <thomas.stratmann@9elements.com>
2014-12-10 17:21:23 +0100

    helpful comment

--- bash/chruby.sh
+++ bash/chruby.sh
@@ -3,6 +3,7 @@
 source /usr/local/share/chruby/chruby.sh
 source /usr/local/share/chruby/auto.sh
 
+# My prompt uses this. It falls back when the function does not exist.
 function which_ruby(){
   chruby | grep \* | cut -b 4-
 }
